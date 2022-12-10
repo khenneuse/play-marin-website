4000aff6ac4fe95c2cbd85a1daa8043657b705ed
Kent Henneuse <kent@henneuse.org>
2022-12-10 11:17:16 -0800

    Make inputs required and disable submit on click

--- scripts/actions.js
+++ scripts/actions.js
@@ -8,5 +8,19 @@ function menuToggle() {
 }
 
 function goToDonationPage() {
-  location.href='https://donate.playmarin.org/944598-holidaydonate';
+  location.href = "https://donate.playmarin.org/944598-holidaydonate";
+}
+
+function enableSubmit() {
+  let inputs = document.querySelectorAll("input[required]");
+  let submitButton = document.querySelector('input[type="submit"]');
+  let buttonDisabled = false;
+  for (var i = 0; i < inputs.length; i++) {
+    let changedInput = inputs[i];
+    if (changedInput.value.trim() === "" || changedInput.value === null) {
+      buttonDisabled = true;
+      break;
+    }
+  }
+  submitButton.disabled = buttonDisabled;
 }
