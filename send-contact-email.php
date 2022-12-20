ea9b60bb049b1166bf02da0726b8885be0855291
Kent Henneuse <kent@henneuse.org>
2022-12-19 19:33:16 -0800

    fix of the elements

--- send-contact-email.php
+++ send-contact-email.php
@@ -38,9 +38,6 @@ $raw_message = wordwrap($raw_message, 70);
 
 $ini_array = parse_ini_file($CONTACT_INI);
 $to_emails = $ini_array['to_name'];
-$to_names = $ini_array['to_name'];
-$to_name   = $to_emails[0];
-$to_email  = $to_emails[0];
 $bcc_names  = isset($ini_array['bcc_name']) ? $ini_array['bcc_name'] : [];
 $bcc_emails = isset($ini_array['bcc_email']) ? $ini_array['bcc_email'] : [];
 
@@ -53,15 +50,18 @@ Phone: $phone
 $raw_message
 ET;
 
+$to_email_array = array();
 $headers[] = "From: $from <$from_email>";
 for ($index = 0; $index < count($to_emails); $index++) {
-  $headers[] = "To: $to_names[$index] <$to_emails[$index]>";
+  $email = "$to_names[$index] <$to_emails[$index]>";
+  $headers[] = "To: $email";
+  $to_email_array[] = $email;
 }
 for ($index = 0; $index < count($bcc_emails); $index++) {
   $headers[] = "bcc: $bcc_names[$index] <$bcc_emails[$index]>";
 }
 
-mail($to_email,$subject,$message, implode("\r\n", $headers));
+mail(implode(",",$to_email), $subject,$message, implode("\r\n", $headers));
 
 echo "Email successfully sent\n";
 header("Location: index.html");
